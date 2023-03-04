55f651322d99e6c7fa53b55badab034c0e037580
Kurt Stephens <git@kurtstephens.com>
2023-03-03 22:12:47 -0600

    Checkin examples.

---
+++ tmp/test/example1-test.sql
@@ -0,0 +1,33 @@
+-- ----------------------------------------------------
+-- example1-test.sql:
+
+DROP EXTENSION example1;
+CREATE EXTENSION example1;
+
+DROP TABLE IF EXISTS cubics;
+CREATE TABLE cubics (
+  cubics_id int,
+  c0 float8,
+  c1 float8,
+  c2 float8,
+  c3 float8
+);
+INSERT INTO cubics VALUES
+  (1, 2.0, 3.0, 5.0, 7.0),
+  (2, 2.3, 5.7, 11.13, 17.23),
+  (3, -5.2, 1.2, -99.0, 12.34);
+
+DROP TABLE IF EXISTS cubic_parameters;
+CREATE TABLE cubic_parameters (
+  x float8
+);
+INSERT INTO cubic_parameters VALUES
+  (2),
+  (-3.7),
+  (3.1415926);
+
+SELECT *, cubic_poly(x, c0, c1, c2, c3)
+FROM cubics, cubic_parameters;
+
+-- ----------------------------------------------------
+
