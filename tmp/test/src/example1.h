f413cdf0961abbc1c69b9d6a88f46d51e5ca61a7
Kurt Stephens <git@kurtstephens.com>
2023-03-05 15:24:09 -0600

    Move tmp/test to swig-101/lab/pg.

--- tmp/test/src/example1.h
+++
@@ -1,7 +0,0 @@
-#define EXAMPLE1_VERSION "1.2.3"
-/* Returns: c0 + c1*x + c2*x^2 + c3*x^3 */
-double cubic_poly(double x,
-                  double c0,
-                  double c1,
-                  double c2,
-                  double c3);
