a09e23006aba13139398530e766d7d1302d2fedd
Kurt Stephens <git@kurtstephens.com>
2024-01-10 20:53:34 -0600

    postgresql: use snprintf().

--- Source/Modules/postgresql.cxx
+++ Source/Modules/postgresql.cxx
@@ -644,7 +644,7 @@ public:
 
       // Now register the function
       char temp[256];
-      sprintf(temp, "%d", numargs);
+      snprintf(temp, sizeof(temp), "%d", numargs);
       if (exporting_destructor) {
 	Printf(init_func_def, "SWIG_TypeClientData(SWIGTYPE%s, (void *) %s);\n", swigtype_ptr, wname);
       }
